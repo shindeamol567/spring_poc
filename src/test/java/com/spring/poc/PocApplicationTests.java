a98b308e2036133808bfb36dd1fade078b05eed7
shindeamol567 <shindeamol567@gmail.com>
2020-04-24 08:16:29 -0700

    initial commit.

---
+++ src/test/java/com/spring/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
